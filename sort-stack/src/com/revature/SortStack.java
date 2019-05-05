29d25c3552674e615da0a90fedd7d4aa0bda4ea2
Tucker <tramitchell100@gmail.com>
2019-05-04 21:43:51 -0400

    added comments to SortStack

--- sort-stack/src/com/revature/SortStack.java
+++ sort-stack/src/com/revature/SortStack.java
@@ -4,26 +4,28 @@ import java.util.Stack;
 
 public class SortStack
 {
-	static Stack<Integer> stack = new Stack<>();
+	static Stack<Integer> testStack = new Stack<>();
 
 	public static void main(String[] args)
 	{
-
-		stack.push(5);
-		stack.push(10);
-		stack.push(9);
-		stack.push(2);
-		stack.push(9);
-		stack.push(12);
-		stack.push(-2);
-		stack.push(15);
-		stack.push(6);
-		stack.push(3);
+		// populate stack
+		testStack.push(5);
+		testStack.push(10);
+		testStack.push(9);
+		testStack.push(2);
+		testStack.push(9);
+		testStack.push(12);
+		testStack.push(-2);
+		testStack.push(15);
+		testStack.push(6);
+		testStack.push(3);
 		
-		sort(stack);
-
-		while(!stack.empty())
-			System.out.println(stack.pop());
+		// sort stack
+		sort(testStack);
+		
+		// display stack
+		while(!testStack.empty())
+			System.out.println(testStack.pop());
 
 	}
 
@@ -57,33 +59,54 @@ public class SortStack
 		}
 		
 		// make the stack point to the sortedStack
-		stack = sortedStack;
+		testStack = sortedStack;
 	}
 
-
+	// find the index of the max value of the stack
+	// zero-indexed
+	// works by popping data into a temporary stack,
+	// comparing each datum to the largest found so far,
+	// and then pushing everything back into the original
 	public static int indexOfMax(Stack<Integer> stack)
 	{
 		if (stack.empty())
 			return -1;
 
+		// initialize all variables
+		// max will hold the maximum value that has been found so far
 		Integer max = stack.peek();
+		// index of the maximum value that has been found so far
 		int indexOfMax = 0;
+		// currentIndex will increment once per loop
+		// just to keep track of the size of the stack
 		int currentIndex = 0;
+		// temp stack will be filled with the content of parameter stack
 		Stack<Integer> tempStack = new Stack<>();
 
+		// loop while the stack still has contents
+		// once loop is finished all contents will be in tempStack
 		while (!stack.empty())
 		{
+			// get the value at the top of the stack
 			Integer current = stack.pop();
+			
+			// compare the current item to the maximum so far
 			if (current > max)
 			{
+				// if a larger value is found
+				// update max with that larger value
+				// update index as well
 				max = current;
 				indexOfMax = currentIndex;
 			}
+			// store datum in tempStack
+			// increment index
 			tempStack.push(current);
 			currentIndex++;
 
 		}
-
+		
+		// take stored values and put everything back into the original
 		for (int i = 0; i < currentIndex; i++)
 			stack.push(tempStack.pop());
 
